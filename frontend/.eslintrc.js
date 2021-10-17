da40261d6ac2b820546d4ae88e0b46c400152ea5
Lylio <cxz@gmx.com>
2021-10-17 22:07:57 +0100

    restore and restructure of frontend

--- frontend/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['react-app', 'react-app/jest', 'prettier'],
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': 'warn',
-    'no-console': 'warn',
-    '@typescript-eslint/no-explicit-any': 'warn',
-  },
-};
