18ccb8d5e939571fb902b49e6dd7edbeb6995ad7
Lylio <cxz@gmx.com>
2021-11-23 12:18:34 +0000

    Axios BASE_URL adjusted

--- src/main/webapp/src/taskboard/TaskService.ts
+++ src/main/webapp/src/taskboard/TaskService.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import {TaskboardItem, TaskboardItemStatus} from './TaskboardTypes';
 
-const BASE_URL = 'https://tasket-demo.herokuapp.com';
+const production  = 'https://tasket-demo.herokuapp.com';
+const development = 'http://localhost:8080/';
+const BASE_URL = (process.env.NODE_ENV ? production : development);
 
 export const createTask = (task: { description: string; position: number; id: string; title: string; status: TaskboardItemStatus }) => axios.post<TaskboardItem>(`${BASE_URL}/tasks`, task);
 
