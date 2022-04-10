fdba7cf03c3035e5d5cfeb43d36808be6eec442d
lylio <cxz@gmx.com>
2022-04-10 13:37:55 +0100

    CORS URL update

--- src/main/webapp/src/taskboard/TaskService.ts
+++ src/main/webapp/src/taskboard/TaskService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {TaskboardItem, TaskboardItemStatus} from './TaskboardTypes';
 
-const production  = 'https://tasket-demo.herokuapp.com';
+const production  = 'https://tasket.lyle.app';
 const development = 'http://localhost:8080';
 const BASE_URL = (production);
 
